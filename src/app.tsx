b42a48b2e43c246be015a0c990546ad31a0a739d
Omar Hefnawi <omarhefnawi@gmail.com>
2021-08-07 15:45:44 -0700

    refactoring data retrieval into a hook

--- src/app.tsx
+++ src/app.tsx
@@ -4,18 +4,6 @@ import * as ReactDOM from 'react-dom'
 import {useVirtual} from "react-virtual";
 import {useDebounce} from "use-debounce"
 
-//Remove pagination as it isn't needed
-document.querySelectorAll(".paging").forEach(x => x.outerHTML = null);
-
-//Remove Card Table
-let element = document.querySelector(".cardItemTable");
-element.outerHTML = "<div class='cardItemTable'></div>";
-element = document.querySelector(".cardItemTable");
-
-//Get total results
-const numberOfResults = Number.parseInt(/(?<=\()\d+/.exec(document.querySelector("#ctl00_ctl00_ctl00_MainContent_SubContent_SubContentHeader_searchTermDisplay").innerHTML)[0]);
-console.log(numberOfResults)
-
 const LoadingRow = (props) => (
     <table {...props}>
         <tbody>
@@ -43,19 +31,30 @@ const LoadingRow = (props) => (
         </tbody>
     </table>)
 
-function MagicInfinite() {
-    const DEFAULT_ROW_HEIGHT = 150;
+function setupMagicGathererSearchResultsPage() {
+    //Get total results
+    const numberOfResults = Number.parseInt(/(?<=\()\d+/.exec(document.querySelector("#ctl00_ctl00_ctl00_MainContent_SubContent_SubContentHeader_searchTermDisplay").innerHTML)[0]);
+
+    //Remove pagination as it isn't needed
+    document.querySelectorAll(".paging").forEach(x => x.outerHTML = null);
+
+    //Remove Card Table
+    let mountingElement = document.querySelector(".cardItemTable");
+    mountingElement.outerHTML = "<div class='cardItemTable'></div>";
+    mountingElement = document.querySelector(".cardItemTable");
+    return {numberOfResults, mountingElement};
+}
+
+let {numberOfResults, mountingElement} = setupMagicGathererSearchResultsPage();
+
+type MagicCardElementsAndState = [Array<Element>, ((value: (((prevState: Array<Element>) => Array<Element>) | Array<Element>)) => void)];
+function useCachedGathererResultsState(windowStartIndex : number, bottomWindowIndex : number) : MagicCardElementsAndState {
     const DEFAULT_PAGE_RESULTS_SIZE = 100;
+    const CARD_ROW_SELECTOR = ".cardItemTable table";
 
-    const resultsViewRef = React.useRef();
-    const resultsVirtualizer = useVirtual({
-        size: numberOfResults,
-        estimateSize: useCallback(() => DEFAULT_ROW_HEIGHT, []),
-        parentRef: resultsViewRef
-    });
-
-    const [[topIndex, bottomIndex]] = useDebounce([resultsVirtualizer?.virtualItems[0]?.index, resultsVirtualizer?.virtualItems[resultsVirtualizer.virtualItems.length - 1]?.index], 500);
-    const [cardResultsCache, setCardResultsCache] = useState<Array<Element>>(new Array(numberOfResults));
+    const [[topIndex, bottomIndex]] = useDebounce([windowStartIndex, bottomWindowIndex], 500);
+    const cardDataAndStateSetter: MagicCardElementsAndState = useState<Array<Element>>(new Array(numberOfResults));
+    const [cardResultsCache, setCardResultsCache] = cardDataAndStateSetter;
     const [loadedPages, setLoadedPages] = useState<Set<Number>>(new Set());
 
     useEffect(() => {
@@ -68,25 +67,24 @@ function MagicInfinite() {
             const {origin, pathname, search} = window.location
 
             const urlSearchParams = new URLSearchParams(search);
-            urlSearchParams.set("page", (pageIndex ).toString());
+            urlSearchParams.set("page", (pageIndex).toString());
 
             const response = await fetch(`${origin}${pathname}?${urlSearchParams}`);
             const htmlText = await response.text();
 
             const document = new DOMParser().parseFromString(htmlText, "text/html");
 
-            const tableElements: NodeListOf<Element> = document.querySelectorAll(".cardItemTable table");
+            const tableElements: NodeListOf<Element> = document.querySelectorAll(CARD_ROW_SELECTOR);
             const results = Array.from(tableElements);
 
             const updatedArray = new Array(numberOfResults);
 
             //Merge past results with the results we retrieved, TODO: there could be a library for this
-            for(let index = 0; index < cardResults.length; index++ ){
+            for (let index = 0; index < cardResults.length; index++) {
                 const pageIndexOffset = pageIndex * DEFAULT_PAGE_RESULTS_SIZE;
-                if(index >= pageIndexOffset  && index < pageIndexOffset + DEFAULT_PAGE_RESULTS_SIZE ) {
+                if (index >= pageIndexOffset && index < pageIndexOffset + DEFAULT_PAGE_RESULTS_SIZE) {
                     updatedArray[index] = results[index - pageIndexOffset];
-                }
-                else {
+                } else {
                     updatedArray[index] = cardResults[index];
                 }
             }
@@ -106,6 +104,24 @@ function MagicInfinite() {
         loadData();
     }, [topIndex, bottomIndex])
 
+    return cardDataAndStateSetter;
+}
+
+function MagicInfinite() {
+    const DEFAULT_ROW_HEIGHT = 150;
+
+    const resultsViewRef = React.useRef();
+    const resultsVirtualizer = useVirtual({
+        size: numberOfResults,
+        estimateSize: useCallback(() => DEFAULT_ROW_HEIGHT, []),
+        parentRef: resultsViewRef,
+        overscan: 10
+    });
+
+    const windowStartIndex = resultsVirtualizer?.virtualItems[0]?.index;
+    const windowEndIndex = resultsVirtualizer?.virtualItems[resultsVirtualizer.virtualItems.length - 1]?.index;
+    const [cardResultsCache, _] = useCachedGathererResultsState(windowStartIndex, windowEndIndex);
+
     const [stickiedCardsCache, setStickiedCards] = useState<Array<Element>>([]);
     let stickiedViewRef = useRef();
     const stickiedVirtualizer = useVirtual({
@@ -116,6 +132,27 @@ function MagicInfinite() {
 
     return (
         <>
+            <div ref={resultsViewRef} style={{ height: `800px`, overflow: "auto" }}>
+                <div style={{ height: `${resultsVirtualizer.totalSize}px`, width: "100%", position: "relative" }}>
+                    {resultsVirtualizer.virtualItems.map(virtualRow => (
+                        <div key={virtualRow.index} ref={virtualRow.measureRef}
+                             style={{
+                                 position: "absolute",
+                                 top: 0,
+                                 left: 0,
+                                 width: "100%",
+                                 transform: `translateY(${virtualRow.start}px)` }}>
+                            {
+                                cardResultsCache[virtualRow.index]
+                                    ? <div
+                                        onClick={() => setStickiedCards([...stickiedCardsCache, cardResultsCache[virtualRow.index]])}
+                                        dangerouslySetInnerHTML={{__html: cardResultsCache[virtualRow.index]?.outerHTML}}/>
+                                    : <LoadingRow style={{height: DEFAULT_ROW_HEIGHT}}/>
+                            }
+                        </div>
+                    ))}
+                </div>
+            </div>
             <div ref={stickiedViewRef} style={{ height: `800px`, overflow: "auto" }}>
                 <div style={{ height: `${stickiedVirtualizer.totalSize}px`, width: "100%", position: "relative" }}>
                     {stickiedVirtualizer.virtualItems.map(virtualRow => (
@@ -137,31 +174,10 @@ function MagicInfinite() {
                     ))}
                 </div>
             </div>
-            <div ref={resultsViewRef} style={{ height: `800px`, overflow: "auto" }}>
-                <div style={{ height: `${resultsVirtualizer.totalSize}px`, width: "100%", position: "relative" }}>
-                    {resultsVirtualizer.virtualItems.map(virtualRow => (
-                        <div key={virtualRow.index} ref={virtualRow.measureRef}
-                             style={{
-                                position: "absolute",
-                                top: 0,
-                                left: 0,
-                                width: "100%",
-                                transform: `translateY(${virtualRow.start}px)` }}>
-                            {
-                                cardResultsCache[virtualRow.index]
-                                    ? <div
-                                        onClick={() => setStickiedCards([...stickiedCardsCache, cardResultsCache[virtualRow.index]])}
-                                        dangerouslySetInnerHTML={{__html: cardResultsCache[virtualRow.index]?.outerHTML}}/>
-                                    : <LoadingRow style={{height: DEFAULT_ROW_HEIGHT}}/>
-                            }
-                        </div>
-                    ))}
-                </div>
-            </div>
         </>);
 };
 
-ReactDOM.render((<MagicInfinite/>), element)
+ReactDOM.render((<MagicInfinite/>), mountingElement)
 
 //@ts-ignore
 if (module.hot) {
