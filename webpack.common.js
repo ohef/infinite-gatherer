7dc51615685c9d21476cf3eee7bee2c0312ed2b6
Omar Hefnawi <omarhefnawi@gmail.com>
2021-08-09 11:34:55 -0700

    renaming root entry to index

--- webpack.common.js
+++ webpack.common.js
@@ -1,7 +1,7 @@
 const path = require('path');
 
 module.exports = {
-    entry: path.join(__dirname, 'src', 'app.tsx'),
+    entry: path.join(__dirname, 'src', 'index.tsx'),
     output: {
         path: path.join(__dirname, 'dist'),
         publicPath: '/',
