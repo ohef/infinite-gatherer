af254f4477a37dd73e553554a35e127387d71b54
Omar Hefnawi <omarhefnawi@gmail.com>
2021-08-10 00:03:50 -0700

    theming top bar

--- src/components/MagicInfinite/MagicInifinite.tsx
+++ src/components/MagicInfinite/MagicInifinite.tsx
@@ -8,7 +8,16 @@ import useCachedGathererResultsState from "../../hooks/useCachedGathererResultsS
 import GathererRow from "./GathererRow/GathererRow";
 import LoadingRow from "./LoadingRow/LoadingRow";
 import jss from "jss";
-import {Box, Button, FormControlLabel, Paper, Switch, Typography} from "@material-ui/core";
+import {
+    Box,
+    Button,
+    Grid,
+    createTheme,
+    FormControlLabel,
+    Paper,
+    Switch
+} from "@material-ui/core";
+import {ThemeProvider} from "@material-ui/styles"
 import downloadAsFile from "download-as-file"
 
 const styles = {
@@ -56,6 +65,16 @@ const useGathererSearchResultsWithInteraction = (windowStartIndex: number, windo
     return {cardResultsCache, stickiedCards, stickiedCardsMap, stickyListClickHandler, mainListClickHandler};
 };
 
+const theme = createTheme({
+    palette: {
+        background: {paper: "#3c3c3c"},
+        primary: {
+            main: '#ff3d00'
+        },
+        text: {primary: "#FFFFFF"}
+    }
+})
+
 const MagicInfinite : React.FC<{}> = () => {
     const resultsViewRef = React.useRef();
     const resultsVirtualizer = useVirtual({
@@ -86,50 +105,59 @@ const MagicInfinite : React.FC<{}> = () => {
     });
 
     return (
-        <>
-            <Box pb={2}>
-                <Paper variant={"outlined"} elevation={0}>
-                    <Box pl={2}>
-                        <FormControlLabel control={<Switch onChange={x => setIsViewingMainList(!isViewingMainList)}/>}
-                                          label="Show selected list"/>
-                        {isViewingMainList ? null :
-                            <Button variant={"outlined"} onClick={() => downloadAsFile({
-                                data: stickiedCards.map(ele => ele.querySelector("span.cardTitle > a").innerHTML).join("\n"),
-                                filename: 'stickied.txt'
-                            })}>Download</Button>
-                        }
-                    </Box>
-                </Paper>
-            </Box>
-            { isViewingMainList
-                ?
-                <div ref={resultsViewRef} style={{height: `75vh`, overflow: "auto"}}>
-                    <div style={{height: `${resultsVirtualizer.totalSize}px`, width: "100%", position: "relative"}}>
-                        {resultsVirtualizer.virtualItems.map(virtualRow => (
-                            <div key={virtualRow.index} ref={virtualRow.measureRef}
-                                 style={{
-                                     position: "absolute",
-                                     top: 0,
-                                     left: 0,
-                                     width: "100%",
-                                     transform: `translateY(${virtualRow.start}px)`
-                                 }}>
-                                {
-                                    cardResultsCache[virtualRow.index]
-                                        ? <GathererRow
-                                            rowClassNames={(`${cardResultsCache[virtualRow.index].querySelector(".cardItem").className} ${stickiedCardsMap.get(virtualRow.index) ? classes.selectedRow : ""}`)}
-                                            leftContent={cardResultsCache[virtualRow.index].querySelector(`.leftCol`)}
-                                            middleContent={cardResultsCache[virtualRow.index].querySelector(`.middleCol`)}
-                                            rightContent={cardResultsCache[virtualRow.index].querySelector(`.rightCol`)}
-                                            onRowClickHandler={mainListClickHandler(virtualRow)}/>
-                                        : <LoadingRow/>
-                                }
-                            </div>
-                        ))}
+        <ThemeProvider theme={theme}>
+            <>
+                <Box pb={2}>
+                    <Paper variant={"outlined"} elevation={0}>
+                        <Box margin={1}>
+                            <Grid container>
+                                <Grid item >
+                                    <Box pl={2}>
+                                        <FormControlLabel control={<Switch color={"primary"}
+                                                                           onChange={x => setIsViewingMainList(!isViewingMainList)}/>}
+                                                          label="Show selected list"/>
+                                    </Box>
+                                </Grid>
+                                <Grid item >
+                                    {isViewingMainList ? null :
+                                        <Button color={"primary"} variant={"contained"} onClick={() => downloadAsFile({
+                                            data: stickiedCards.map(ele => ele.querySelector("span.cardTitle > a").innerHTML).join("\n"),
+                                            filename: 'stickied.txt'
+                                        })}>Download List</Button>
+                                    }
+                                </Grid>
+                            </Grid>
+                        </Box>
+                    </Paper>
+                </Box>
+                {isViewingMainList
+                    ?
+                    <div ref={resultsViewRef} style={{height: `75vh`, overflow: "auto"}}>
+                        <div style={{height: `${resultsVirtualizer.totalSize}px`, width: "100%", position: "relative"}}>
+                            {resultsVirtualizer.virtualItems.map(virtualRow => (
+                                <div key={virtualRow.index} ref={virtualRow.measureRef}
+                                     style={{
+                                         position: "absolute",
+                                         top: 0,
+                                         left: 0,
+                                         width: "100%",
+                                         transform: `translateY(${virtualRow.start}px)`
+                                     }}>
+                                    {
+                                        cardResultsCache[virtualRow.index]
+                                            ? <GathererRow
+                                                rowClassNames={(`${cardResultsCache[virtualRow.index].querySelector(".cardItem").className} ${stickiedCardsMap.get(virtualRow.index) ? classes.selectedRow : ""}`)}
+                                                leftContent={cardResultsCache[virtualRow.index].querySelector(`.leftCol`)}
+                                                middleContent={cardResultsCache[virtualRow.index].querySelector(`.middleCol`)}
+                                                rightContent={cardResultsCache[virtualRow.index].querySelector(`.rightCol`)}
+                                                onRowClickHandler={mainListClickHandler(virtualRow)}/>
+                                            : <LoadingRow/>
+                                    }
+                                </div>
+                            ))}
+                        </div>
                     </div>
-                </div>
-                :
-                <>
+                    :
                     <div ref={stickiedViewRef} style={{height: `75vh`, overflow: "auto"}}>
                         <div
                             style={{height: `${stickiedVirtualizer.totalSize}px`, width: "100%", position: "relative"}}>
@@ -153,9 +181,10 @@ const MagicInfinite : React.FC<{}> = () => {
                             ))}
                         </div>
                     </div>
-                </>
-            }
-        </>);
+                }
+            </>
+        </ThemeProvider>
+    );
 };
 
 export default MagicInfinite;
